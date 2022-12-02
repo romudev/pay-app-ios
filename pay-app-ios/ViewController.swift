18d269afd9a1a0742ee811876cdd9c47ea83dbd3
Romulo Sanchez <65043355+romudev@users.noreply.github.com>
2022-12-01 23:34:41 -0600

    fp

---
+++ pay-app-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pay-app-ios
+//
+//  Created by romudev on 25/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
